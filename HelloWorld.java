f8b3abbf38fae46ec8820689272afe6f411427be
MadridMiranda <madrid2005miranda@gmail.com>
2026-02-19 08:18:06 -0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// Beecrowd | 1000 - Hello World!
+// Para o Beecrowd use a clase Main
+// public class Main
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
