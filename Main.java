4a51513bbced6cf7fbca902143952f25e86f9b7c
MadridMiranda <madrid2005miranda@gmail.com>
2026-02-19 08:12:49 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
-// Beecrowd | 1000 - Hello World!
+// Beecrowd | 1001 - Extremamente Básico
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main (String [] args){
+        Scanner leitura = new Scanner(System.in);
+        int a = leitura.nextInt();
+        int b = leitura.nextInt();
+        int x = a + b;
+        System.out.printf("X = %d\n", x);
+        leitura.close();
     }
 }
