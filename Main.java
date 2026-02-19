80ee227a71f5f4635bdc0b187e4ec34cd0ea855d
MadridMiranda <madrid2005miranda@gmail.com>
2026-02-19 08:06:00 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String [] args){
+        Scanner leitura = new Scanner(System.in);
+        int a = leitura.nextInt();
+        int b = leitura.nextInt();
+        int x = a + b;
+        System.out.printf("X = %d\n", x);
+        leitura.close();
+    }
+}
