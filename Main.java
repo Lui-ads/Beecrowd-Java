211c7c4f81902d91df9016ee4370ddf5fac65c6d
MadridMiranda <madrid2005miranda@gmail.com>
2026-03-04 10:21:06 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+// 1003 - Simple Sum
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner entrada = new Scanner(System.in);
+        int a = entrada.nextInt();
+        int b = entrada.nextInt();
+        int soma = a + b;
+        System.out.printf("SOMA = %d\n", soma);
+
+    }
+
+}
